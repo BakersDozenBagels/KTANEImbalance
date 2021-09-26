6a25c4d1c2b083fca8f836d87bdd4293b996f31d
BakersDozenBagels <Bakersdozenbagelsbergman@gmail.com>
2021-09-26 12:05:43 -0400

    [V1.2] Initial commit

---
+++ Assets/Extensions.cs
@@ -0,0 +1,56 @@
+﻿namespace Imbalance
+{
+    public static class Extensions
+    {
+        public static string ToImbalance(this int c)
+        {
+            c &= 255;
+            string rep = ".«";
+            if((c & 1) == 1)
+                rep = "1" + rep;
+            else
+            {
+                rep = "2" + rep;
+                c -= 2;
+            }
+            c >>= 1;
+            for(int i = 1; i < 15; i += 2)
+            {
+                int code = (c & 1) + (c & 2);
+                switch(code)
+                {
+                    case 0:
+                        rep = "12" + rep;
+                        break;
+                    case 1:
+                        rep = "11" + rep;
+                        break;
+                    case 2:
+                        rep = "22" + rep;
+                        break;
+                    case 3:
+                        rep = "21" + rep;
+                        break;
+                }
+                c >>= 2;
+            }
+
+            string rep1 = rep;
+            string rep2 = rep;
+            if(rep1.StartsWith("2"))
+                rep1 = rep1.Substring(1);
+            if(rep2.StartsWith("1"))
+                rep2 = rep2.Substring(1);
+
+            while(rep1.StartsWith("12"))
+                rep1 = rep1.Substring(2);
+            while(rep2.StartsWith("21"))
+                rep2 = rep2.Substring(2);
+
+            rep1 = "«" + rep1;
+            rep2 = "»" + rep2;
+
+            return rep1.Length < rep2.Length ? rep1 : rep2;
+        }
+    }
+}
---
+++ Assets/ImbalanceScript.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using Imbalance;
+using System.Collections;
+
+public class ImbalanceScript : MonoBehaviour
+{
+    [SerializeField]
+    private TextMesh _textA, _textB, _buttonText;
+    [SerializeField]
+    private GameObject _cap;
+    [SerializeField]
+    private KMSelectable _button;
+    [SerializeField]
+    private KMBombModule _module;
+    [SerializeField]
+    private KMBombInfo _info;
+    [SerializeField]
+    private KMAudio _audio;
+
+    private static int _idCounter;
+    private int _chosenA, _chosenB, _solution, _id;
+    private bool _isSolved;
+
+    private void Start()
+    {
+        _id = ++_idCounter;
+        _chosenA = Random.Range(0, 128);
+        _chosenB = Random.Range(0, 128);
+        _textA.text = _chosenA.ToImbalance();
+        _textB.text = _chosenB.ToImbalance();
+        //Debug.LogFormat("[Imbalance #{0}] The top screen is displaying: \"{1}\"", _id, _textA.text);
+        //Debug.LogFormat("[Imbalance #{0}] The bottom screen is displaying: \"{1}\"", _id, _textB.text);
+
+        _solution = _chosenA * _chosenB;
+        //Debug.LogFormat("[Imbalance #{0}] The expected solution is: \"{1}\"", _id, _solution);
+
+        _button.OnInteract += () => Press();
+        _button.OnInteractEnded += () => StartCoroutine(Animate(false));
+    }
+
+    private IEnumerator Animate(bool v)
+    {
+        if(v)
+            _audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, _button.transform);
+        else
+            _audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonRelease, _button.transform);
+
+        float time = Time.time;
+        while(Time.time < time + 0.125f)
+        {
+            _cap.transform.localPosition = new Vector3(0f, v ? -0.55f * (Time.time - time) / 0.125f : -0.55f + (Time.time - time) / 0.25f, 0f);
+            yield return null;
+        }
+        _cap.transform.localPosition = new Vector3(0f, v ? -0.55f : 0f, 0f);
+    }
+
+    private void Update()
+    {
+        _buttonText.text = ((int)_info.GetTime() % 10).ToString();
+    }
+
+    private bool Press()
+    {
+        StartCoroutine(Animate(true));
+        if(_isSolved)
+            return false;
+        if(_textA.text.EndsWith(".«"))
+            _textA.text = _buttonText.text;
+        else
+            _textA.text += _buttonText.text;
+
+        if(!_solution.ToString().StartsWith(_textA.text))
+        {
+            //Debug.LogFormat("[Imbalance #{0}] You entered \"{1}\", which was incorrect.", _id, _textA.text);
+            _module.HandleStrike();
+            _textA.text = _chosenA.ToImbalance();
+        }
+        if(_solution.ToString() == _textA.text)
+        {
+            //Debug.LogFormat("[Imbalance #{0}] You entered \"{1}\", which was correct. Solved.", _id, _textA.text);
+            _module.HandlePass();
+            _isSolved = true;
+            StartCoroutine(FadeOut());
+        }
+
+        return false;
+    }
+
+    private IEnumerator FadeOut()
+    {
+        float time = Time.time;
+        while(Time.time - time < 1f)
+        {
+            _textA.color = new Color(1f, 1f, 1f, 1f - (Time.time - time));
+            _textB.color = new Color(1f, 1f, 1f, 1f - (Time.time - time));
+            _buttonText.color = new Color(1f, 1f, 1f, 1f - (Time.time - time));
+            yield return null;
+        }
+        _textA.color = new Color(1f, 1f, 1f, 0f);
+        _textB.color = new Color(1f, 1f, 1f, 0f);
+        _buttonText.color = new Color(1f, 1f, 1f, 0f);
+    }
+}
